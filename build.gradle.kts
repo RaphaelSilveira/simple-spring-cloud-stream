1a4ff0263a874f3c085181a13eff9b1c8ed42e9b
Raphael Silveira <wid_rsilveira@uolinc.com>
2019-08-01 16:04:45 -0300

    Remoção de arquivos desnecessários e criação do docker compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,25 @@
+import org.gradle.api.JavaVersion.VERSION_12
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.1.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
-    kotlin("jvm") version "1.2.71"
-    kotlin("plugin.spring") version "1.2.71"
+    kotlin("jvm") version "1.3.41"
+    kotlin("plugin.spring") version "1.3.41"
 }
 
 group = "br.com.silveira.raphael"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = VERSION_12
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+    compile {
+        exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+    }
+}
 
 repositories {
     mavenCentral()
@@ -20,6 +30,7 @@ extra["springCloudVersion"] = "Greenwich.SR2"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-undertow")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -43,6 +54,6 @@ dependencyManagement {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "12"
     }
 }
